2daf46ac8c7cd5952a612f5065477a4a3d8f86e5
Dimitri ONGOUA <ongouadimitri5@gmail.com>
2021-05-22 01:06:41 +0100

    init project

---
+++ app/src/main/java/info/mediafresh/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package info.mediafresh.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
